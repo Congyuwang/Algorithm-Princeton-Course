9617bf18806425c79c48bb5c474f4a5d349f5bf0
Congyuwang <116020237@link.cuhk.edu.cn>
2020-04-03 22:26:18 +0800

    Add LinkedStackOfStrings.java

---
+++ unionfind/src/princeton/algorithm/stack/LinkedStackOfStrings.java
@@ -0,0 +1,31 @@
+package princeton.algorithm.stack;
+
+/**
+ * This is an implementation of linked stack of strings
+ */
+public class LinkedStackOfStrings {
+
+    private Node first = null;
+
+    private class Node {
+        String item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void push(String item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+    }
+
+    public String pop() {
+        String item = first.item;
+        first = first.next;
+        return item;
+    }
+}
