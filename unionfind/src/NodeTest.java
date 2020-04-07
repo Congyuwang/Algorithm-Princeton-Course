bf3c6366088a04f0bf6509fbc40b5fd2c92001c4
Congyuwang <116020237@link.cuhk.edu.cn>
2020-04-07 23:46:05 +0800

    Add ArrayQueueOfStrings

---
+++ unionfind/src/NodeTest.java
@@ -0,0 +1,26 @@
+import princeton.algorithm.queue.LinkedQueueOfStrings;
+import princeton.algorithm.queue.ArrayQueueOfStrings;
+import java.util.Scanner;
+
+public class NodeTest {
+    public static void main(String[] args) {
+        LinkedQueueOfStrings linkedStack  = new LinkedQueueOfStrings();
+        ArrayQueueOfStrings arrayStack = new ArrayQueueOfStrings();
+        Scanner scanner = new Scanner(System.in);
+        // End input with EOF!
+        while (scanner.hasNext()) {
+            String item = scanner.next();
+            if (item.equals("-")) {
+                try {
+                    System.out.printf("arrayStack: %s\n", arrayStack.dequeue());
+                    System.out.printf("linkedStack: %s\n", linkedStack.dequeue());
+                } catch (Exception e) {
+                    System.out.printf("Error: %s\n", e.getMessage());
+                }
+            } else {
+                arrayStack.enqueue(item);
+                linkedStack.enqueue(item);
+            }
+        }
+    }
+}
---
+++ unionfind/src/princeton/algorithm/queue/ArrayQueueOfStrings.java
@@ -0,0 +1,61 @@
+package princeton.algorithm.queue;
+
+import java.util.Arrays;
+
+public class ArrayQueueOfStrings {
+    // count represents the total number of items
+    // head represents the position of the first item
+    // tail represents the position of the next item to be filled
+    int count = 0;
+    int head = 0;
+    int tail = 0;
+    String[] s = new String[1];
+
+    public void enqueue(String item) {
+        if (tail == s.length) {
+            tail = 0;
+        }
+        s[tail++] = item;
+        count++;
+        if (count == s.length) {
+            resize(s.length * 2);
+        }
+        System.out.printf("head: %d, tail: %d, count: %d\n", head, tail, count);
+        System.out.println(Arrays.toString(s));
+    }
+
+    public String dequeue() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("StackUnderFlow!");
+        }
+        if (head == s.length) {
+            head = 0;
+        }
+        String item = s[head];
+        s[head++] = null;
+        count--;
+        if (count == s.length / 4) {
+            resize(s.length / 2);
+        }
+        System.out.printf("head: %d, tail: %d, count: %d\n", head, tail, count);
+        System.out.println(Arrays.toString(s));
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    private void resize(int capacity) {
+        String[] copy = new String[capacity];
+        if (head > tail) {
+            System.arraycopy(s, 0, copy, 0, tail);
+            System.arraycopy(s, head, copy, tail, s.length - head);
+        } else {
+            System.arraycopy(s, head, copy, 0, count);
+        }
+        head = 0;
+        tail = count;
+        s = copy;
+    }
+}
