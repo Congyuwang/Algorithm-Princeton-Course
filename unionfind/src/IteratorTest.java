cb3a6b03a80c0eff39308cebe85ab6f10ec95bf2
Congyuwang <116020237@link.cuhk.edu.cn>
2020-04-14 15:07:39 +0800

    Add twoStackQueue to iteratorTest

--- unionfind/src/IteratorTest.java
+++ unionfind/src/IteratorTest.java
@@ -1,15 +1,19 @@
+import princeton.algorithm.queue.Queue;
+import princeton.algorithm.stack.Stack;
 import princeton.algorithm.queue.ArrayQueue;
 import princeton.algorithm.queue.LinkedQueue;
+import princeton.algorithm.queue.TwoStackQueue;
 import princeton.algorithm.stack.ArrayStack;
 import princeton.algorithm.stack.LinkedStack;
 import java.util.Scanner;
 
 public class IteratorTest {
     public static void main(String[] args) {
-        LinkedQueue<Integer> linkedQueue  = new LinkedQueue<>();
-        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
-        LinkedStack<Integer> linkedStack = new LinkedStack<>();
-        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        Queue<Integer> linkedQueue  = new LinkedQueue<>();
+        Queue<Integer> arrayQueue = new ArrayQueue<>();
+        Queue<Integer> twoStackQueue = new TwoStackQueue<>();
+        Stack<Integer> linkedStack = new LinkedStack<>();
+        Stack<Integer> arrayStack = new ArrayStack<>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String item = scanner.next();
@@ -17,6 +21,7 @@ public class IteratorTest {
                 try {
                     System.out.printf("linkedQueue: %s\n", linkedQueue.dequeue());
                     System.out.printf("arrayQueue: %s\n", arrayQueue.dequeue());
+                    System.out.printf("twoStackQueue: %s\n", twoStackQueue.dequeue());
                     System.out.printf("linkedStack: %d\n", linkedStack.pop());
                     System.out.printf("arrayStack: %d\n", arrayStack.pop());
                 } catch (Exception e) {
@@ -30,6 +35,11 @@ public class IteratorTest {
                 System.out.println();
                 System.out.println("arrayQueue (FIFO):");
                 for (int i : arrayQueue) {
+                    System.out.print(i + " ");
+                }
+                System.out.println();
+                System.out.println("twoStackQueue (FIFO):");
+                for (int i : twoStackQueue) {
                     System.out.print(i + " ");
                 }
                 System.out.println();
@@ -48,6 +58,7 @@ public class IteratorTest {
                     int intItem = Integer.parseInt(item);
                     linkedQueue.enqueue(intItem);
                     arrayQueue.enqueue(intItem);
+                    twoStackQueue.enqueue(intItem);
                     arrayStack.push(intItem);
                     linkedStack.push(intItem);
                 } catch (NumberFormatException e) {
