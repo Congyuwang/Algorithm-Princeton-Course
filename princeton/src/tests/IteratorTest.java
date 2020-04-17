2cdd5a4a8b2f96683a42b39e2d35fd43f2b7039d
Congyuwang <116020237@link.cuhk.edu.cn>
2020-04-17 10:19:12 +0800

    add deque to iteratorTest

--- princeton/src/tests/IteratorTest.java
+++ princeton/src/tests/IteratorTest.java
@@ -1,11 +1,12 @@
 package tests;
 
 import princeton.algo.queue.Queue;
-import princeton.algo.queue.RandomizedQueue;
 import princeton.algo.stack.Stack;
-import princeton.algo.queue.ArrayQueue;
 import princeton.algo.queue.LinkedQueue;
+import princeton.algo.queue.ArrayQueue;
 import princeton.algo.queue.TwoStackQueue;
+import princeton.algo.queue.Deque;
+import princeton.algo.queue.RandomizedQueue;
 import princeton.algo.stack.ArrayStack;
 import princeton.algo.stack.LinkedStack;
 import java.util.Scanner;
@@ -15,6 +16,7 @@ class IteratorTest {
         Queue<Integer> linkedQueue = new LinkedQueue<>();
         Queue<Integer> arrayQueue = new ArrayQueue<>();
         Queue<Integer> twoStackQueue = new TwoStackQueue<>();
+        Queue<Integer> deque = new Deque<>();
         Queue<Integer> randomizedQueue = new RandomizedQueue<>();
         Stack<Integer> linkedStack = new LinkedStack<>();
         Stack<Integer> arrayStack = new ArrayStack<>();
@@ -25,13 +27,38 @@ class IteratorTest {
             if (item.equals("-")) {
                 try {
                     System.out.printf("linkedQueue: %s\n", linkedQueue.dequeue());
+                } catch (Exception e) {
+                    System.out.printf("linkedQueue Error: %s\n", e.getMessage());
+                }
+                try {
                     System.out.printf("arrayQueue: %s\n", arrayQueue.dequeue());
+                } catch (Exception e) {
+                        System.out.printf("arrayQueue Error: %s\n", e.getMessage());
+                }
+                try {
                     System.out.printf("twoStackQueue: %s\n", twoStackQueue.dequeue());
+                } catch (Exception e) {
+                        System.out.printf("twoStackQueue Error: %s\n", e.getMessage());
+                }
+                try {
+                    System.out.printf("deque: %s\n", deque.dequeue());
+                } catch (Exception e) {
+                        System.out.printf("deque Error: %s\n", e.getMessage());
+                }
+                try {
                     System.out.printf("randomizedQueue: %s\n", randomizedQueue.dequeue());
+                } catch (Exception e) {
+                        System.out.printf("randomizedQueue Error: %s\n", e.getMessage());
+                }
+                try {
                     System.out.printf("linkedStack: %d\n", linkedStack.pop());
+                } catch (Exception e) {
+                        System.out.printf("linkedStack Error: %s\n", e.getMessage());
+                }
+                try {
                     System.out.printf("arrayStack: %d\n", arrayStack.pop());
                 } catch (Exception e) {
-                    System.out.printf("Error: %s\n", e.getMessage());
+                        System.out.printf("arrayStack Error: %s\n", e.getMessage());
                 }
             } else if (item.equals(">>")) {
                 System.out.println("linkedQueue (FIFO):");
@@ -46,6 +73,11 @@ class IteratorTest {
                 System.out.println();
                 System.out.println("twoStackQueue (FIFO):");
                 for (int i : twoStackQueue) {
+                    System.out.print(i + " ");
+                }
+                System.out.println();
+                System.out.println("deque (FIFO):");
+                for (int i : deque) {
                     System.out.print(i + " ");
                 }
                 System.out.println();
@@ -70,6 +102,7 @@ class IteratorTest {
                     linkedQueue.enqueue(intItem);
                     arrayQueue.enqueue(intItem);
                     twoStackQueue.enqueue(intItem);
+                    deque.enqueue(intItem);
                     randomizedQueue.enqueue(intItem);
                     arrayStack.push(intItem);
                     linkedStack.push(intItem);
