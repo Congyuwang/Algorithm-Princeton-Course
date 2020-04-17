e2a17252bae7fff5f3f2d82431078cf47c505ebd
Congyuwang <116020237@link.cuhk.edu.cn>
2020-04-17 12:30:45 +0800

    Extract test method from SortTest

--- princeton/src/tests/SortTest.java
+++ princeton/src/tests/SortTest.java
@@ -9,45 +9,42 @@ import edu.princeton.cs.algs4.Stopwatch;
 class SortTest {
     public static void main(String[] args) {
         int LENGTH = 20000;
+        Random random = new Random();
         Double[] test1 = new Double[LENGTH];
         Double[] test2 = new Double[LENGTH];
-        Double[] test1copy1 = new Double[LENGTH];
-        Double[] test2copy1 = new Double[LENGTH];
-        Random random = new Random();
         for (int i = 0; i < LENGTH; i++) {
             test1[i] = random.nextDouble();
-            test2[i] = random.nextDouble();
-            test2[i] += i * 0.2;
+            test2[i] = random.nextDouble() + i * 0.2;
         }
-        System.arraycopy(test1, 0, test1copy1, 0, LENGTH);
-        System.arraycopy(test2, 0, test2copy1, 0, LENGTH);
 
         // test1: Selection sort
-        System.out.println("Test1: Selection sort:");
-        Stopwatch timer1 = new Stopwatch();
-        Selection.sort(test1);
-        System.out.printf("elapsed time = %4f\n", timer1.elapsedTime());
-        System.out.println("Is sorted?: " + Util.isSorted(test1));
-
-        // test1: Insertion sort
-        System.out.println("Test1: Insertion sort:");
-        Stopwatch timer2 = new Stopwatch();
-        Insertion.sort(test1copy1);
-        System.out.printf("elapsed time = %4f\n", timer2.elapsedTime());
-        System.out.println("Is sorted?: " + Util.isSorted(test2));
+        System.out.println("Test1 (random order):");
+        test("selection", test1);
+        test("insertion", test1);
 
         // test2: Insertion sort (partially sorted)
-        System.out.println("Test2 (partially sorted): Insertion sort:");
-        Stopwatch timer3 = new Stopwatch();
-        Selection.sort(test2);
-        System.out.printf("elapsed time = %4f\n", timer3.elapsedTime());
-        System.out.println("Is sorted?: " + Util.isSorted(test2));
+        System.out.println("Test2 (partially sorted):");
+        test("selection", test2);
+        test("insertion", test2);
+    }
 
-        // test2: Selection sort (partially sorted)
-        System.out.println("Test2 (partially sorted): Insertion sort:");
-        Stopwatch timer4 = new Stopwatch();
-        Insertion.sort(test2copy1);
-        System.out.printf("elapsed time = %4f\n", timer4.elapsedTime());
-        System.out.println("Is sorted?: " + Util.isSorted(test2));
+    private static <T extends Comparable<? super T>> void test(String algorithm, T[] test) {
+        // Caveat: be careful about the clone() here
+        T[] testCopy = test.clone();
+        System.out.printf("    %s sort: ", algorithm);
+        Stopwatch timer = new Stopwatch();
+        switch (algorithm) {
+            case "selection":
+                Selection.sort(testCopy);
+                break;
+            case "insertion":
+                Insertion.sort(testCopy);
+                break;
+            default:
+                break;
+        }
+        assert(Util.isSorted(testCopy));
+        System.out.printf("elapsed time = %.4f", timer.elapsedTime());
+        System.out.println();
     }
 }
