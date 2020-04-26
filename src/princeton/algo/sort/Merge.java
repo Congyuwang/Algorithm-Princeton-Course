0d83f5055132ea07cc4e8c811fcc3909cd732b40
Congyuwang <116020237@link.cuhk.edu.cn>
2020-04-26 17:57:59 +0800

    reduce arrayCopy times

--- src/princeton/algo/sort/Merge.java
+++ src/princeton/algo/sort/Merge.java
@@ -11,6 +11,7 @@ public class Merge {
     public static <T extends Comparable<? super T>> void sort(T[] a) {
         @SuppressWarnings("unchecked")
         T[] aux = (T[]) Array.newInstance(a.getClass().getComponentType(), a.length);
+        System.arraycopy(a, 0, aux, 0, a.length);
         sort(a, aux, 0, a.length);
     }
 
@@ -20,9 +21,9 @@ public class Merge {
             return;
         }
         int mid = (lo + hi) >>> 1;
-        sort(a, aux, lo, mid);
-        sort(a, aux, mid, hi);
-        if (!Util.less(a[mid], a[mid - 1])) {
+        sort(aux, a, lo, mid);
+        sort(aux, a, mid, hi);
+        if (!Util.less(aux[mid], aux[mid - 1])) {
             return;
         }
         merge(a, aux, lo, mid, hi);
@@ -31,16 +32,15 @@ public class Merge {
     private static <T extends Comparable<? super T>> void merge(T[] a, T[] aux, int lo, int mid, int hi) {
         assert Util.isSorted(a, lo, mid);
         assert Util.isSorted(a, mid, hi);
-        System.arraycopy(a, lo, aux, lo, hi - lo);
         for (int k = lo, i = lo, j = mid; k < hi; k++) {
             if (i == mid) {
-                a[k] = aux[j++];
+                aux[k] = a[j++];
             } else if (j == hi) {
-                a[k] = aux[i++];
-            } else if (Util.less(aux[i], aux[j])) {
-                a[k] = aux[i++];
+                aux[k] = a[i++];
+            } else if (Util.less(a[i], a[j])) {
+                aux[k] = a[i++];
             } else {
-                a[k] = aux[j++];
+                aux[k] = a[j++];
             }
         }
         assert Util.isSorted(a, lo, hi);
