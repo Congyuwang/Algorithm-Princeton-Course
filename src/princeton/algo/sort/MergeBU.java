bd53f1e33088ce993c25c18910921d7e4b5995c9
Congyuwang <116020237@link.cuhk.edu.cn>
2020-04-27 10:33:34 +0800

    optimize MergeBU

--- src/princeton/algo/sort/MergeBU.java
+++ src/princeton/algo/sort/MergeBU.java
@@ -4,25 +4,54 @@ import java.lang.reflect.Array;
 
 public class MergeBU {
 
+    private static final int CUTOFF = 7;
+
     private MergeBU() {};
 
     public static <T extends Comparable<? super T>> void sort(T[] a) {
+        int length = a.length;
         @SuppressWarnings("unchecked")
-        T[] aux = (T[]) Array.newInstance(a.getClass().getComponentType(), a.length);
-        System.arraycopy(a, 0, aux, 0, a.length);
+        T[] aux = (T[]) Array.newInstance(a.getClass().getComponentType(), length);
+        System.arraycopy(a, 0, aux, 0, length);
+
+        for (int lo = 0; lo < length; lo += CUTOFF) {
+            Insertion.sort(aux, lo, Math.min(length, lo + CUTOFF));
+        }
+
         int round = 0;
-        for (int size = 1; size < a.length; size = size << 1) {
-            for (int lo = 0; lo < a.length - size; lo += 2 * size) {
-                if (round % 2 == 0) {
-                    merge(a, aux, lo, lo + size, Math.min(a.length, lo + size * 2));
-                } else {
-                    merge(aux, a, lo, lo + size, Math.min(a.length, lo + size * 2));
+        for (int size = CUTOFF; size < length; size = size << 1) {
+            int lo;
+            if (round % 2 == 0) {
+                for (lo = 0; lo < length - size; lo += 2 * size) {
+                    int mid = lo + size;
+                    int hi = Math.min(length, lo + size * 2);
+                    if (Util.less(aux[mid], aux[mid - 1])) {
+                        merge(a, aux, lo, mid, hi);
+                    } else {
+                        System.arraycopy(aux, lo, a, lo, hi - lo);
+                    }
+                }
+                if (lo < length) {
+                    System.arraycopy(aux, lo, a, lo, length - lo);
+                }
+            } else {
+                for (lo = 0; lo < length - size; lo += 2 * size) {
+                    int mid = lo + size;
+                    int hi = Math.min(length, lo + size * 2);
+                    if (Util.less(a[mid], a[mid - 1])) {
+                        merge(aux, a, lo, mid, hi);
+                    } else {
+                        System.arraycopy(a, lo, aux, lo, hi - lo);
+                    }
+                }
+                if (lo < length) {
+                    System.arraycopy(a, lo, aux, lo, length - lo);
                 }
             }
             round++;
         }
         if (round % 2 == 0) {
-            System.arraycopy(aux, 0, a, 0, a.length);
+            System.arraycopy(aux, 0, a, 0, length);
         }
     }
 
