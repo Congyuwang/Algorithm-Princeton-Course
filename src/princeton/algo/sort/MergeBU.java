5797b1bc5c690681f87866dcf3d06cd45a65054a
Congyuwang <116020237@link.cuhk.edu.cn>
2020-04-27 17:12:17 +0800

    fix MergeBU bug

--- src/princeton/algo/sort/MergeBU.java
+++ src/princeton/algo/sort/MergeBU.java
@@ -18,21 +18,29 @@ public class MergeBU {
             Insertion.sort(a, lo, Math.min(length, lo + CUTOFF));
         }
 
+        int round = 0;
         for (int size = CUTOFF; size < length; size <<= 1) {
-            int twiceSize = size * 2;
             T[] temp = a;
             a = aux;
             aux = temp;
-            mergeTo(aux, a, size, twiceSize);
+            mergeTo(aux, a, size);
+            round++;
+        }
+
+        assert Util.isSorted(a);
+
+        if (round % 2 == 1) {
+            System.arraycopy(a, 0, aux, 0, length);
         }
     }
 
-    private static <T extends Comparable<? super T>> void mergeTo(T[] src, T[] dest, int size, int twiceSize) {
+    private static <T extends Comparable<? super T>> void mergeTo(T[] src, T[] dest, int size) {
         int length = src.length;
         int lo;
+        int twiceSize = size * 2;
         for (lo = 0; lo < length - size; lo += twiceSize) {
             int mid = lo + size;
-            int hi = Math.min(length, lo + size * 2);
+            int hi = Math.min(length, lo + twiceSize);
             if (Util.less(src[mid], src[mid - 1])) {
                 Merge.merge(src, dest, lo, mid, hi);
             } else {
