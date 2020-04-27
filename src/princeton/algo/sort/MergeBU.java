7d922c79baf2f73d04c034ab9478111975406134
Congyuwang <116020237@link.cuhk.edu.cn>
2020-04-27 16:38:38 +0800

    Simplify codes

--- src/princeton/algo/sort/MergeBU.java
+++ src/princeton/algo/sort/MergeBU.java
@@ -14,72 +14,33 @@ public class MergeBU {
         T[] aux = (T[]) Array.newInstance(a.getClass().getComponentType(), length);
         System.arraycopy(a, 0, aux, 0, length);
 
-        int round = 0;
-        for (int temp = CUTOFF; temp < length; temp <<= 1) {
-            round++;
-        }
-        round %= 2;
-
-        if (round == 0) {
-            for (int lo = 0; lo < length; lo += CUTOFF) {
-                Insertion.sort(a, lo, Math.min(length, lo + CUTOFF));
-            }
-        } else {
-            for (int lo = 0; lo < length; lo += CUTOFF) {
-                Insertion.sort(aux, lo, Math.min(length, lo + CUTOFF));
-            }
+        for (int lo = 0; lo < length; lo += CUTOFF) {
+            Insertion.sort(a, lo, Math.min(length, lo + CUTOFF));
         }
 
         for (int size = CUTOFF; size < length; size <<= 1) {
             int twiceSize = size * 2;
-            if (round == 0) {
-                int lo;
-                for (lo = 0; lo < length - size; lo += twiceSize) {
-                    int mid = lo + size;
-                    int hi = Math.min(length, lo + size * 2);
-                    if (Util.less(a[mid], a[mid - 1])) {
-                        merge(aux, a, lo, mid, hi);
-                    } else {
-                        System.arraycopy(a, lo, aux, lo, hi - lo);
-                    }
-                }
-                if (lo < length) {
-                    System.arraycopy(a, lo, aux, lo, length - lo);
-                }
-                round = 1;
-            } else {
-                int lo;
-                for (lo = 0; lo < length - size; lo += twiceSize) {
-                    int mid = lo + size;
-                    int hi = Math.min(length, lo + size * 2);
-                    if (Util.less(aux[mid], aux[mid - 1])) {
-                        merge(a, aux, lo, mid, hi);
-                    } else {
-                        System.arraycopy(aux, lo, a, lo, hi - lo);
-                    }
-                }
-                if (lo < length) {
-                    System.arraycopy(aux, lo, a, lo, length - lo);
-                }
-                round = 0;
-            }
+            T[] temp = a;
+            a = aux;
+            aux = temp;
+            mergeTo(aux, a, size, twiceSize);
         }
     }
 
-    private static <T extends Comparable<? super T>> void merge(T[] a, T[] aux, int lo, int mid, int hi) {
-        assert Util.isSorted(a, lo, mid);
-        assert Util.isSorted(a, mid, hi);
-        for (int k = lo, i = lo, j = mid; k < hi; k++) {
-            if (i == mid) {
-                a[k] = aux[j++];
-            } else if (j == hi) {
-                a[k] = aux[i++];
-            } else if (Util.less(aux[i], aux[j])) {
-                a[k] = aux[i++];
+    private static <T extends Comparable<? super T>> void mergeTo(T[] src, T[] dest, int size, int twiceSize) {
+        int length = src.length;
+        int lo;
+        for (lo = 0; lo < length - size; lo += twiceSize) {
+            int mid = lo + size;
+            int hi = Math.min(length, lo + size * 2);
+            if (Util.less(src[mid], src[mid - 1])) {
+                Merge.merge(src, dest, lo, mid, hi);
             } else {
-                a[k] = aux[j++];
+                System.arraycopy(src, lo, dest, lo, hi - lo);
             }
         }
-        assert Util.isSorted(a, lo, hi);
+        if (lo < length) {
+            System.arraycopy(src, lo, dest, lo, length - lo);
+        }
     }
 }
