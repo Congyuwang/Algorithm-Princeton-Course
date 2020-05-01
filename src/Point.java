fbd055a6365c5d737d4c947fe5a9b501a8aee5b7
Congyuwang <116020237@link.cuhk.edu.cn>
2020-05-01 14:39:08 +0800

    fix negative 0.0 problem

--- src/Point.java
+++ src/Point.java
@@ -67,7 +67,7 @@ public class Point implements Comparable<Point> {
                 return Double.POSITIVE_INFINITY;
             }
         }
-        return (double) 0.0 + (that.y - this.y) / (that.x - this.x);
+        return 0.0 + (double) (that.y - this.y) / (that.x - this.x);
     }
 
     /**
