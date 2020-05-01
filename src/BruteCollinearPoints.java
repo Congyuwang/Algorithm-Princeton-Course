8b7cea9b28d8ca184e0be0b06a80de25d0574c9f
Congyuwang <116020237@link.cuhk.edu.cn>
2020-05-01 09:18:09 +0800

    add CollinearPoints

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,100 @@
+
+public class BruteCollinearPoints {
+
+    private int numberOfSegments;
+
+    private Node firstLine = null;
+
+    private class Node {
+        LineSegment lineSegment;
+        Node next;
+    }
+
+    /**
+     * Does not apply to cases where there are five or more collinear points
+     */
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException("null input");
+        }
+        int numberOfPoints = points.length;
+        for (int i1 = 0; i1 < numberOfPoints; i1++) {
+            for (int i2 = i1 + 1; i2 < numberOfPoints; i2++) {
+                for (int i3 = i2 + 1; i3 < numberOfPoints; i3++) {
+                    for (int i4 = i3 + 1; i4 < numberOfPoints; i4++) {
+                        if (points[i1] == null) {
+                            throw new IllegalArgumentException("null point");
+                        }
+                        double slope1 = points[i1].slopeTo(points[i2]);
+                        double slope2 = points[i1].slopeTo(points[i3]);
+                        double slope3 = points[i1].slopeTo(points[i4]);
+                        if (slope1 == slope2 && slope1 == slope3) {
+                            System.out.println("i1: " + i1 + " i2: " + i2 + " i3: " + i3 + " i4: " + i4);
+                            Point min = points[i1];
+                            Point max = points[i1];
+                            int temp = min.compareTo(points[i2]);
+                            if (temp > 0) {
+                                min = points[i2];
+                            } else if (temp == 0) {
+                                throw new IllegalArgumentException("repeated points");
+                            }
+                            temp = max.compareTo(points[i2]);
+                            if (temp < 0) {
+                                max = points[i2];
+                            } else if (temp == 0) {
+                                throw new IllegalArgumentException("repeated points");
+                            }
+                            temp = min.compareTo(points[i3]);
+                            if (temp > 0) {
+                                min = points[i3];
+                            } else if (temp == 0) {
+                                throw new IllegalArgumentException("repeated points");
+                            }
+                            temp = max.compareTo(points[i3]);
+                            if (temp < 0) {
+                                max = points[i3];
+                            } else if (temp == 0) {
+                                throw new IllegalArgumentException("repeated points");
+                            }
+                            temp = min.compareTo(points[i4]);
+                            if (temp > 0) {
+                                min = points[i4];
+                            } else if (temp == 0) {
+                                throw new IllegalArgumentException("repeated points");
+                            }
+                            temp = max.compareTo(points[i4]);
+                            if (temp < 0) {
+                                max = points[i4];
+                            } else if (temp == 0) {
+                                throw new IllegalArgumentException("repeated points");
+                            }
+                            if (numberOfPoints == 0) {
+                                firstLine = new Node();
+                                firstLine.lineSegment = new LineSegment(min, max);
+                            } else {
+                                Node oldFirst = firstLine;
+                                firstLine = new Node();
+                                firstLine.lineSegment = new LineSegment(min, max);
+                                firstLine.next = oldFirst;
+                            }
+                            numberOfSegments++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public int numberOfSegments() {
+        return numberOfSegments;
+    }
+
+    public LineSegment[] segments() {
+        LineSegment[] lineSegments = new LineSegment[numberOfSegments];
+        for (int i = 0; i < numberOfSegments; i++) {
+            lineSegments[i] = firstLine.lineSegment;
+            firstLine = firstLine.next;
+        }
+        return lineSegments;
+    }
+}
---
+++ src/FastCollinearPoints.java
@@ -0,0 +1,95 @@
+import java.util.Arrays;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+public class FastCollinearPoints {
+
+    private final int TRHESH_HOLD = 4;
+
+    private int numberOfSegments;
+
+    private Node firstLine = null;
+
+    private class Node {
+        LineSegment lineSegment;
+        Node next;
+    }
+
+    public FastCollinearPoints(Point[] points) {
+        Point[] pointsCopy = points.clone();
+        Arrays.sort(pointsCopy);
+        for (Point p : pointsCopy) {
+            Arrays.sort(points);
+            Arrays.sort(points, p.slopeOrder());
+            int count = 0;
+            int head = 0;
+            double slopeMem = p.slopeTo(points[0]);
+            for (int tail = 1; tail < points.length; tail++) {
+                double thisSlope = p.slopeTo(points[tail]);
+                if (slopeMem == thisSlope) {
+                    count++;
+                } else {
+                    if (count >= TRHESH_HOLD - 2 && p.compareTo(points[head]) <= 0) {
+                        if (numberOfSegments == 0) {
+                            firstLine = new Node();
+                            firstLine.lineSegment = new LineSegment(points[head], points[tail - 1]);
+                        } else {
+                            Node oldFirst = firstLine;
+                            firstLine = new Node();
+                            firstLine.lineSegment = new LineSegment(points[head], points[tail - 1]);
+                            firstLine.next = oldFirst;
+                        }
+                        numberOfSegments++;
+                    }
+                    head = tail;
+                    count = 0;
+                }
+                slopeMem = thisSlope;
+            }
+        }
+    }
+
+    public int numberOfSegments() {
+        return numberOfSegments;
+    }
+
+    public LineSegment[] segments() {
+        LineSegment[] lineSegments = new LineSegment[numberOfSegments];
+        for (int i = 0; i < numberOfSegments; i++) {
+            lineSegments[i] = firstLine.lineSegment;
+            firstLine = firstLine.next;
+        }
+        return lineSegments;
+    }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
+}
