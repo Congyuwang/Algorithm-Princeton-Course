1b8b79146fb697e47363e8fc4c822896ce4ad795
Congyuwang <116020237@link.cuhk.edu.cn>
2020-06-09 19:38:17 +0800

    finish Board.java

--- src/assignments/eightPuzzle/Board.java
+++ src/assignments/eightPuzzle/Board.java
@@ -1,23 +1,62 @@
 package assignments.eightPuzzle;
 
+import java.util.ArrayDeque;
 import java.util.Iterator;
 
 public class Board {
 
     private final int[][] tiles;
     private final int n;
+    private final int[] zero;
+    private final int hammingDistance;
+    private final int manhattanDistance;
 
-    // create a board from an n-by-n array of tiles,
-    // where tiles[row][col] = tile at (row, col)
+    /**
+     * Create a board, calculate its hammingDistance, manhattanDistance, find the
+     * zero position. Finish all these ar construction to prevent recalculation. The
+     * constructor takes O(n^2) time.
+     */
     public Board(int[][] tiles) {
-        this.tiles = tiles.clone();
+        if (tiles.length < 2 || tiles.length != tiles[0].length) {
+            throw new IllegalArgumentException("illegal board shape");
+        }
         this.n = tiles.length;
+        this.tiles = makeClone(tiles, n);
+        int hamming = -1;
+        int pos = 1;
+        int manhattan = 0;
+        int[] zero = null;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (this.tiles[i][j] != pos++) {
+                    hamming++;
+                    manhattan += manhattan(i, j, this.tiles[i][j], n);
+                }
+                if (zero == null && this.tiles[i][j] == 0) {
+                    zero = new int[] {i, j};
+                }
+            }
+        }
+        this.zero = zero;
+        this.hammingDistance = hamming;
+        this.manhattanDistance = manhattan;
+    }
+
+    /**
+     * Note that the private constructor does NOT make a clone of tiles
+     */
+    private Board(int[][] tiles, int hamming, int manhattan, int[] zero) {
+        this.n = tiles.length;
+        this.tiles = tiles;
+        this.zero = zero;
+        this.hammingDistance = hamming;
+        this.manhattanDistance = manhattan;
     }
 
     // string representation of this board
     public String toString() {
-        final int padding = 2 + (int) Math.max(Math.floor(Math.log10(n * n)), 0);
-        final String digitFormat = String.format("%%dd", padding);
+        final int padding = 2 + (int) Math.max(Math.floor(Math.log10(n * n - 1)), 0);
+        final String digitFormat = "%" + padding + "d";
         final StringBuilder stringBuilder = new StringBuilder();
 
         stringBuilder.append(String.valueOf(n));
@@ -31,49 +70,46 @@ public class Board {
         return stringBuilder.toString();
     }
 
-    // board dimension n
-    public int dimension() {
+    /**
+     * return the size (height) of the board. Constant run time.
+     *
+     * @return the height of the square board
+     */
+    public final int dimension() {
         return n;
     }
 
-    // number of tiles out of place
-    public int hamming() {
-        int count = -1;
-        int pos = 1;
-        for (int[] row : tiles) {
-            for (int i : row) {
-                if (i != pos++) {
-                    count++;
-                }
-            }
-        }
-        return count;
+    /**
+     * calculate the hamming distance (the number of tiles out of place) between
+     * this board and the goal board. Constant run time.
+     *
+     * @return the hamming distance
+     */
+    public final int hamming() {
+        return hammingDistance;
     }
 
-    private int manhattan(int row, int col, int val) {
-        if (val == 0) {
-            return 0;
-        }
-        return Math.abs((val - 1) / n - row) + Math.abs((val - 1) % n - col);
+    /**
+     * calculate the manhattan distance (the sum of manhattan distances of each tile
+     * to its goal position) between this board and the goal board. Constant run
+     * time.
+     *
+     * @return the manhattan distance
+     */
+    public final int manhattan() {
+        return manhattanDistance;
     }
 
-    // sum of Manhattan distances between tiles and goal
-    public int manhattan() {
-        int manhattan = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                manhattan += manhattan(i, j, tiles[i][j]);
-            }
-        }
-        return manhattan;
+    /**
+     * check whether this board is the goal board. Constant run time.
+     */
+    public final boolean isGoal() {
+        return manhattanDistance == 0;
     }
 
-    // is this board the goal board?
-    public boolean isGoal() {
-        return hamming() == 0;
-    }
-
-    // does this board equal y?
+    /**
+     * check whether this board equals to another board. O(n^2) time.
+     */
     @Override
     public boolean equals(Object y) {
         if (y == null || !(y instanceof Board)) {
@@ -90,36 +126,100 @@ public class Board {
         return true;
     }
 
-    // all neighboring boards
+    /**
+     * return a iterator giving all neighbors of a board.
+     * O(n^2) time for cloning the board.
+     */
     public Iterable<Board> neighbors() {
+
+        final ArrayDeque<int[]> neighbors = new ArrayDeque<>(4);
+
+        if (zero[0] > 0) {
+            neighbors.add(new int[] { -1, 0 });
+        }
+
+        if (zero[1] > 0) {
+            neighbors.add(new int[] { 0, -1 });
+        }
+
+        if (zero[0] < n - 1) {
+            neighbors.add(new int[] { 1, 0 });
+        }
+
+        if (zero[1] < n - 1) {
+            neighbors.add(new int[] { 0, 1 });
+        }
+
         return new Iterable<Board>() {
 
-			@Override
-			public Iterator<Board> iterator() {
-				return new Iterator<Board>() {
+            @Override
+            public Iterator<Board> iterator() {
+                return new Iterator<Board>() {
+
                     @Override
                     public boolean hasNext() {
-                        // TODO Auto-generated method stub
-                        return false;
+                        return !neighbors.isEmpty();
                     }
+
                     @Override
                     public Board next() {
-                        // TODO Auto-generated method stub
-                        return null;
+                        int[] move = neighbors.poll();
+                        int[][] tilesCopy = makeClone(tiles, n);
+                        int oldVal = tilesCopy[move[0] + zero[0]][move[1] + zero[1]];
+
+                        // exchange zero with another position
+                        tilesCopy[zero[0]][zero[1]] = oldVal;
+                        tilesCopy[move[0] + zero[0]][move[1] + zero[1]] = 0;
+
+                        // update information
+                        int oldManhattan = manhattan(move[0] + zero[0], move[1] + zero[1], oldVal, n);
+                        int newManhattan = manhattan(zero[0], zero[1], oldVal, n);
+                        int deltaHamming = (int) (Math.signum(newManhattan) - Math.signum(oldManhattan));
+                        return new Board(tilesCopy, deltaHamming + hammingDistance,
+                                newManhattan - oldManhattan + manhattanDistance,
+                                new int[] { move[0] + zero[0], move[1] + zero[1] });
                     }
                 };
-			}
+            }
         };
     }
 
-    // a board that is obtained by exchanging any pair of tiles
-    public Board twin() {
-        return new Board(new int[][]{{}});
+    private static final int manhattan(int row, int col, int val, int n) {
+        if (val == 0) {
+            // skip 0
+            return 0;
+        }
+        return Math.abs((val - 1) / n - row) + Math.abs((val - 1) % n - col);
     }
 
-    // unit testing (not graded)
-    public static void main(String[] args) {
+    private static final int[][] makeClone(int[][] tiles, int n) {
+        int[][] copy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            System.arraycopy(tiles[i], 0, copy[i], 0, n);
+        }
+        return copy;
+    }
 
+    /**
+     * unit testing.
+     *
+     * @param args no args needed
+     */
+    public static void main(String[] args) {
+        int[][] test = { { 3, 2, 1 }, { 4, 0, 8 }, { 5, 7, 6 } };
+        Board board = new Board(test);
+        for (Board b : board.neighbors()) {
+            System.out.println(b);
+        }
+        System.out.println(new Board(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } }).manhattan()); // 0
+        System.out.println(new Board(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } }).hamming()); // 0
+        System.out.println(new Board(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } }).isGoal()); // true
+        System.out.println(new Board(new int[][] { { 1, 3, 2 }, { 4, 5, 6 }, { 0, 8, 7 } }).manhattan()); // 4
+        System.out.println(new Board(new int[][] { { 1, 3, 2 }, { 4, 5, 6 }, { 0, 8, 7 } }).hamming()); // 3
+        for (Board b : new Board(new int[][] { { 1, 3, 2 }, { 4, 5, 6 }, { 0, 8, 7 } }).neighbors()) {
+            System.out.print(b);
+            System.out.println(b.manhattan());
+        }
     }
 
 }
