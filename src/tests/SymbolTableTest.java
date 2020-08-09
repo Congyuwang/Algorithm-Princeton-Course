ae3c5b2640209d645dc6467146fb95b1c7710a94
Congyuwang <116020237@link.cuhk.edu.cn>
2020-08-09 17:24:35 +0800

    add read file to symbol table test

--- src/tests/SymbolTableTest.java
+++ src/tests/SymbolTableTest.java
@@ -1,12 +1,29 @@
 package tests;
 
-import princeton.algo.symbolTable.SequentialSearchST;
 import princeton.algo.symbolTable.SymbolTable;
+import static tests.FrequencyCounter.*;
 
 public class SymbolTableTest {
     public static void main(String[] args) {
-        SymbolTable<String, Integer> test = new SequentialSearchST<>();
         java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        SymbolTable<String, Integer> test = null;
+
+        while (test == null) {
+            try {
+                System.out.println("choose test type: (read/input)");
+                String type = scanner.next();
+                System.out.println("enter table type:");
+                String tableType = scanner.next();
+                if (type.equals("read")) {
+                    test = countWords("data/shorterShakespeare.txt", tableType, 5);
+                } else if (type.equals("input")) {
+                    test = tableChooser(tableType);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         loop: while (true) {
             System.out.println("enter command:");
             String command = scanner.next();
